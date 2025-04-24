86ed68caa5090dd1073a746b3a9a01fb9779a7d9
partzel <ahmedelmahdibendou2003@gmail.com>
2025-04-24 13:46:28 +0100

    feat: add prompt text box

---
+++ PetraqTerminal/PetraqTerminal/Views/UserInputView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PetraqTerminal.Views;
+
+public partial class UserInputView : UserControl
+{
+    public UserInputView()
+    {
+        InitializeComponent();
+    }
+}
